2fc5337d38daf557e378928449c00ea947eb0ce1
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2023-12-25 15:43:56 +0530

    added navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import Logo from "../assets/images/logo.svg";
 
 const Home = ({ setProgress }) => {
   // Top Loading Bar dummy progress in future we will update the progress based on API calls succession or failure
@@ -23,6 +24,7 @@ const Home = ({ setProgress }) => {
   return (
     <>
       <div className="container w-[88%] mx-10 ">
+        {/* Top Links */}
         <div className="links my-3 flex justify-between ">
           <div className="sharelinks flex gap-2">
             <div className="h-7 w-7 rounded-md bg-gray-200 flex justify-center items-center">
@@ -65,6 +67,35 @@ const Home = ({ setProgress }) => {
             </select>
           </div>
         </div>
+        {/* Divider */}
+        <hr className="w-full" />
+        {/* Nav */}
+        <div className="nav my-7 flex justify-between items-center">
+          <div className="text-3xl font-[GilroyB]">SP/ACE</div>
+          <div className="w-[60%] h-[3rem] border rounded-xl relative">
+            <input
+              className="w-full h-full rounded-xl px-4 pr-14"
+              placeholder="Enter your space product..."
+              type="text"
+            />
+            <i className="cursor-pointer ri-search-line absolute right-6 top-2/4 -translate-y-2/4  hover:text-blue-400"></i>
+          </div>
+          <div className="icons flex justify-center items-center gap-5">
+            <div className="flex justify-center items-center relative">
+              <i className="text-4xl ri-user-line"></i>
+            </div>
+            <div className="flex justify-center items-center relative ">
+              <i className="text-4xl ri-heart-3-line "></i>
+              <span className="badge absolute  -top-2 -right-3 text-white bg-red-500 h-5 w-5 text-[10px] rounded-full flex justify-center items-center"><b>2</b></span>
+            </div>
+            <div className="flex justify-center items-center relative">
+              <i className="text-4xl ri-shopping-bag-line"></i>
+              <span className="badge absolute -top-2 -right-3 text-white bg-red-500 h-5 w-5 text-[10px] rounded-full flex justify-center items-center"><b>2</b></span>
+            </div>
+          </div>
+        </div>
+        {/* Divider */}
+        <hr className="w-full" />
       </div>
       <div className="footer"></div>
     </>
