f9b2303cacc111a5ed37468cad13dd4bade2b01d
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2023-12-26 23:14:25 +0530

    fixed footer typing error

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -425,7 +425,7 @@ const Footer = () => {
 
       <div className="p-8 flex flex-col gap-4 items-center ">
         <img className="" src={Payment} alt="payment methods"></img>
-        <p className="text-[#696969] capitalize">Copyright &copy; <a href="#">Space Space</a> all rights reserved.</p>
+        <p className="text-[#696969] capitalize">Copyright &copy; <a href="#">Shop Space</a> all rights reserved.</p>
       </div>
     </div>
   );
