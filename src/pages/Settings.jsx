cf6f6c789f4c23de4d69b7e8d466063227b0eb61
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2024-01-01 22:26:52 +0530

    added account in settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+
+// Page Transition variant import
+import { pageTransitionVariant } from "../constants/Transition";
 
 // images
 import userImage from "../assets/images/womenmodel.png";
@@ -34,7 +38,13 @@ const Settings = ({ setProgress }) => {
   return (
     <>
       {user ? (
-        <div className="settingsContainer w-full flex flex-col h-full">
+        <motion.div
+          variants={pageTransitionVariant}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          className="settingsContainer w-full flex flex-col h-full"
+        >
           {/* Navlink */}
           <div className="settingNav w-full flex justify-between items-center py-5 px-10">
             <h1 className="text-xl font-bold font-[Montserrat] text-blue-600">
@@ -88,20 +98,134 @@ const Settings = ({ setProgress }) => {
               </div>
               <div className="w-[75%] h-[35%] rounded-xl   bg-[#2352FE] mb-5 flex items-center justify-center relative overflow-hidden">
                 <div className="flex flex-col items-center justify-center relative z-20">
-                  <h1 className=" text-lg text-white font-bold ">
-                    Go Premium
-                  </h1>
+                  <h1 className=" text-lg text-white font-bold ">Go Premium</h1>
                   <p className=" text-gray-50 text-xs">Best Quality</p>
-                  <h1 className="cursor-pointer border-2 border-white py-2 px-4 rounded-xl text-white text-sm  mt-2">
-                    Freemium
+                  <h1 className="cursor-pointer border-2 border-white py-2 px-4 rounded-xl text-white text-xs  mt-2">
+                    Enter Beta
                   </h1>
                 </div>
                 <i className="absolute -rotate-45 -right-10 z-10 -top-14 text-9xl text-gray-200 opacity-45 ri-emphasis"></i>
               </div>
             </div>
-            <div className="w-[85%] border-l  h-full"></div>
+            <div className="w-[85%] border-l  h-full">
+              {accountActive ? (
+                <div className="account w-full h-full p-5">
+                  <h1 className="text-3xl font-bold">Account</h1>
+                  <div className="w-full flex gap-16 items-start">
+                    <div className="flex gap-5 flex-col mt-10 justify-center">
+                      <div className=" flex gap-12  justify-between items-center">
+                        <label
+                          className="font-[Montserrat] text-gray-500"
+                          htmlFor="email"
+                        >
+                          Email Address
+                        </label>
+                        <input
+                          className="outline-none border-2 text-lg py-2 rounded-2xl px-5"
+                          type="text"
+                          id="email"
+                          placeholder="Enter email"
+                        />
+                      </div>
+                      <div className=" flex gap-12  justify-between items-center">
+                        <label
+                          className="font-[Montserrat] text-gray-500"
+                          htmlFor="username"
+                        >
+                          Username
+                        </label>
+                        <input
+                          className="outline-none border-2 text-lg py-2 rounded-2xl px-5"
+                          type="text"
+                          id="username"
+                          placeholder="Enter  username"
+                        />
+                      </div>
+                      <div className=" flex gap-12 justify-between items-center">
+                        <label
+                          className="font-[Montserrat] text-gray-500"
+                          htmlFor="pass"
+                        >
+                          Password
+                        </label>
+                        <input
+                          className="outline-none border-2 text-lg py-2 rounded-2xl px-5"
+                          type="text"
+                          id="pass"
+                          placeholder="Enter password"
+                        />
+                      </div>
+                      <div className=" flex gap-12  justify-between items-center">
+                        <label
+                          className="font-[Montserrat] text-gray-500"
+                          htmlFor="confPass"
+                        >
+                          Confirm Password
+                        </label>
+                        <input
+                          className="outline-none border-2 text-lg py-2 rounded-2xl px-5"
+                          type="text"
+                          id="confPass"
+                          placeholder="Confirm password"
+                        />
+                      </div>
+                    </div>
+                    <div className="flex gap-5 flex-col mt-10 justify-center">
+                      <div className=" flex gap-12  justify-between items-center">
+                        <label
+                          className="font-[Montserrat] text-gray-500"
+                          htmlFor="date"
+                        >
+                          Date
+                        </label>
+                        <input
+                          className="pointer-events-none outline-none border-2 text-lg py-2 rounded-2xl px-5"
+                          type="text"
+                          id="date"
+                          value={new Date().toLocaleDateString()}
+                        />
+                      </div>
+                      <div className=" flex gap-12  justify-between items-center">
+                        <label
+                          className="font-[Montserrat] text-gray-500"
+                          htmlFor="location"
+                        >
+                          Location
+                        </label>
+                        <input
+                          className="pointer-events-none outline-none border-2 text-lg py-2 rounded-2xl px-5"
+                          type="text"
+                          id="location"
+                          value={"India"}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full flex mt-5 justify-between">
+                    <motion.div
+                      whileHover={{ scale: 1.015 }}
+                      whileTap={{ scale: 0.99 }}
+                      className="px-5 text-white font-bold font-[Montserrat] text-sm py-4 bg-green-400 cursor-pointer rounded-xl"
+                    >
+                      <p>Save Changes</p>
+                    </motion.div>
+                  </div>
+                  <hr className="bg-gray-500 my-6" />
+                  <h1 className="text-xl font-bold my-3">Danger Zone</h1>
+                  <p className="w-[40%] mb-4 text-sm text-gray-500">
+                    Once you delete your account, there is no going back. You
+                    will lose all golden opportunities, Please be certain.
+                  </p>
+                  <button className="bg-red-300 cursor-pointer hover:bg-red-400 hover:text-red-800 font-bold text-red-500 px-5 py-4 text-center rounded-xl text-sm">
+                    Delete Account
+                  </button>
+                </div>
+              ) : (
+                <div className="profile w-full h-full"></div>
+              )}
+            </div>
           </div>
-        </div>
+        </motion.div>
       ) : (
         navigate("/")
       )}
