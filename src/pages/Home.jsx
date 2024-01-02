920f47af5fc36095a60ec4e80d72c9b348c8c196
khushi kandoi <khushikandoii09@gmail.com>
2024-01-02 16:20:36 +0530

    added wishlist page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -246,14 +246,19 @@ const Home = ({ setProgress }) => {
             >
               <i className="text-4xl ri-settings-4-line"></i>
             </div>
-            <div className="cursor-pointer flex justify-center items-center relative ">
+            <div
+              onClick={() => navigate("/wishlist")}
+              className="cursor-pointer flex justify-center items-center relative "
+            >
               <i className="text-4xl ri-heart-3-line "></i>
               <span className="badge absolute  -top-2 -right-3 text-white bg-red-500 h-5 w-5 text-[10px] rounded-full flex justify-center items-center">
                 <b>2</b>
               </span>
             </div>
             <div
-            onClick={() => navigate("/cart")} className="cursor-pointer flex justify-center items-center relative">
+              onClick={() => navigate("/cart")}
+              className="cursor-pointer flex justify-center items-center relative"
+            >
               <i className="text-4xl ri-shopping-bag-line"></i>
               <span className="badge absolute -top-2 -right-3 text-white bg-red-500 h-5 w-5 text-[10px] rounded-full flex justify-center items-center">
                 <b>2</b>
