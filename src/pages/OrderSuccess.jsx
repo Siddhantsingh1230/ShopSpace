1aa19bb8bed7c2f935f1c04fe646a86028526989
khushi kandoi <khushikandoii09@gmail.com>
2024-01-07 20:11:15 +0530

    added gif in order success

--- src/pages/OrderSuccess.jsx
+++ src/pages/OrderSuccess.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import shoppingBag from "../assets/images/shoppingBag.png";
+import ribbonfalling from "../assets/images/ribbonfalling.gif";
 import { Link } from "react-router-dom";
 
 const OrderSuccess = () => {
@@ -11,17 +12,18 @@ const OrderSuccess = () => {
           <img className="flex w-56" src={shoppingBag} alt="img" />
           <div className="flex flex-col justify-center items-center gap-4 text-sm">
             <p className="text-4xl font-bold">Success!!</p>
-            <div className="flex flex-col items-center text-center max-sm:px-6">
+            <div className="flex flex-col items-center text-center px-36 max-sm:px-6">
             <p>Your Order has been placed successfully.</p>
             <p >
               We will send you a shipping confirmation email as soon as your order
-              ships.
+              ships.You can visit <Link to={"/orders"} className="font-bold hover:underline text-purple-700 hover:text-purple-900">My Orders</Link> to check order details.
             </p>
             </div>
-            <Link to={"/"} className="border-2 border-black rounded-lg p-3 m-4 font-semibold hover:border-4 hover:border-gray-800 cursor-pointer text-md">Continue Shopping</Link>
+            <Link to={"/"} className=" border-2 z-10 border-purple-800 text-purple-800 rounded-lg p-3 m-4 font-semibold hover:border-4 hover:border-purple-800 hover:fony  cursor-pointer text-md">Continue Shopping</Link>
           </div>
         </div>
       </div>
+      <div className=" h-1/2 z-50 w-full absolute" style={{backgroundImage: `url(${ribbonfalling})`, animation: "longfade 10s ease infinite alternate "}}> </div>
     </>
   );
 };
