7e8626def1abfe31fbc465b447f2c7c800f8f8bb
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2024-01-08 13:50:43 +0530

    edited category api little bit

--- src/api/category.js
+++ src/api/category.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getAllCategories = async () => {
   const { data } = await axios.get(
-    process.env.REACT_APP_SERVER_BASE_URL + "/categories"
+    process.env.REACT_APP_SERVER_BASE_URL + "/v1/categories"
   );
   return data.categories;
 };
